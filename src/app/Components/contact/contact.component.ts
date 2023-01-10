27189fc424c4a7c21442fdbd9576ce44f711965e
Saad1430 <chalbhaag.420@gmail.com>
2023-01-10 11:30:52 +0500

    updated to Angular version 15

--- src/app/Components/contact/contact.component.ts
+++ src/app/Components/contact/contact.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  fileName = '';
+  file !:File;
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+  }
+
+  onFileSelected(event:any) {
+    this.file = event.target.files[0];
+
+    if (this.file) {
+      this.fileName = this.file.name;
+      const formData = new FormData();
+      formData.append("image", this.file);
+      console.log(event);
+      console.warn(this.file);
+      
+      const upload$ = this.http.post("http://localhost:3000/artwork/api/", formData, {
+        reportProgress: true,
+        observe: 'events'
+      }).pipe().subscribe(res => console.log(res)
+      );
+
+    }
   }
 
 }
